fa2f68e476d3a396e5917dd8f09609475150f116
AKing-283 <pushpakreddy283@gmail.com>
2024-09-09 10:39:17 +0530

    changes made in  streamlit_app.py

--- weather_app/streamlit_app.py
+++ weather_app/streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
-from weather_app.weather.models import City  # Replace with your model
+from weather.weather_app.weather.models import City  # Replace with your model
+
 
 def main():
     st.title("Your Django App on Streamlit")
 
     model_data = City.objects.all()
     st.write(model_data)
+
 
 if __name__ == "__main__":
     main()
